41fe663bc0459239778764f4eb08f0154ad65100
Jitesh23 <jiteshpowankar@gmail.com>
2018-06-13 19:33:46 +0530

    Details:
    - Install bower and angular
    - Bootstrapped angular application

---
+++ public/application.js
@@ -0,0 +1,9 @@
+var mainApplicationModuleName = 'mean';
+
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+
+angular.element(document).ready(function() {
+
+angular.bootstrap(document, [mainApplicationModuleName]);
+
+});
