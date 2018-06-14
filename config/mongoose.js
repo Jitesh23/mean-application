d293e7f52237c681af42e38818f21a1ada71b21f
Jitesh23 <jiteshpowankar@gmail.com>
2018-06-14 19:19:05 +0530

    Added article model

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,7 +7,7 @@ module.exports = function(){
   var db = mongoose.connect(config.db);
 
   require('../app/models/index.server.model.js')
-
+  require('../app/models/article.server.model');
   return db;
 
 }
